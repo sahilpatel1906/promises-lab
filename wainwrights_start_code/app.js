5f422b44fe178717dc2745e1065b6e6fdb18cc25
Sahil Patel <sahilpatel1906@gmail.com>
2024-03-19 13:42:40 -0500

    added filtering

--- wainwrights_start_code/app.js
+++ wainwrights_start_code/app.js
@@ -3,8 +3,13 @@ let jsonData = {};
 const getAllWaingwrights = async () => {
     const response = await fetch("https://raw.githubusercontent.com/annahndr/annahndr.github.io/master/wainwrights_data/wainwrights.json");
     jsonData = await response.json();
+    displayList(jsonData);
+}
+
+const displayList = (data) => {
     const listOfHills =  document.querySelector("#wainwrights-list");
-    jsonData.forEach((hill) => {
+    listOfHills.innerText = "";
+    data.forEach((hill) => {
         const hillItem = document.createElement("li");
         const hillInfo = document.createElement("p");
         hillInfo.innerText = JSON.stringify(hill);
@@ -22,11 +27,25 @@ const getAllWaingwrights = async () => {
     // })
     // hillItem.appendChild(hillInfo);
     // listOfHills.appendChild(hillItem);
-    
 }
 
-const filterData = () => {
-    const filterName = document.querySelector("#filter-submit")
-}
+const filterNameForm= document.querySelector("#filter-hills-form");
+filterNameForm.addEventListener("submit", (event) => {
+    event.preventDefault();   
+    const filterName = event.target["filter-text"].value
+    if(filterName != ""){
+        filteredData = jsonData.filter((hill) => {
+            const name = hill.name.toLowerCase();
+            if(name.includes(filterName)){
+                return hill
+            }
+        })
+        displayList(filteredData);
+    } else {
+        getAllWaingwrights();  
+    }
+
+    
+})
 
 getAllWaingwrights();
